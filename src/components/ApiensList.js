a1453236446bfb903d37672612fe42452bf3c399
MageForth <mageforth@gmail.com>
2022-06-06 11:08:40 +0430

    :zap: apiens list component scroll fixed

--- src/components/ApiensList.js
+++ src/components/ApiensList.js
@@ -11,33 +11,35 @@ const ApiensList = () => {
   }
 
   return (
-    <div className="apiens__list">
-      {console.log(data[0])}
-      {data.map((item) => (
-        <div key={item.id} className="apiens__list_item">
-          <img src={item.thumbnailUrl} alt="" />
+    <div className="apiens__list_scroll_container">
+      <div className="apiens__list">
+        {console.log(data[0])}
+        {data.map((item) => (
+          <div key={item.id} className="apiens__list_item">
+            <img src={item.thumbnailUrl} alt="" />
 
-          <div className="Rank_Id__container">
-            <span className="Rank">
-              Rank: <span>{item.id}54</span>
-            </span>
-            <span className="Id">#{item.id}213</span>
-          </div>
-
-          <div className="Price_Time__container">
-            <span className="Price">
-              Price: <span>{item.id}.280</span>{' '}
-              <span className="Price__icons">
-                <div></div>
-                <div></div>
+            <div className="Rank_Id__container">
+              <span className="Rank">
+                Rank: <span>{item.id}54</span>
               </span>
-            </span>
-            <span className="Time">
-              Time: <span>{item.id}7 Minutes ago</span>
-            </span>
+              <span className="Id">#{item.id}213</span>
+            </div>
+
+            <div className="Price_Time__container">
+              <span className="Price">
+                Price: <span>{item.id}.280</span>{' '}
+                <span className="Price__icons">
+                  <div></div>
+                  <div></div>
+                </span>
+              </span>
+              <span className="Time">
+                Time: <span>{item.id}7 Minutes ago</span>
+              </span>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
